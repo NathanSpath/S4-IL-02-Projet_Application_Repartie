3bb0f7aceedf1adb1cb7527f37f22e3562751657
Julien VINCENT <julien.vincent7@etu.univ-lorraine.fr>
2026-06-10 11:08:35 +0200

    Ajout des couleurs ainsi que l'affichage des restaurants

---
+++ proxy-java/src/RMI/rmi/ServiceRestaurant.java
@@ -0,0 +1,28 @@
+package RMI.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServiceRestaurant extends Remote {
+
+    /**
+     * Récupère les informations détaillées du restaurant (nom, adresse, etc.).
+     * @return Une chaîne JSON représentant l'objet RMI.model.Restaurant.
+     * @throws RemoteException
+     */
+    String getRestaurants() throws RemoteException;
+
+    /**
+
+     * @param idTab L'identifiant unique de la table à réserver.
+     * @param idCli L'identifiant unique du client qui réserve.
+     * @param nbPers Le nombre de personnes pour la réservation.
+     * @return Une chaîne JSON représentant un booléen (true si la réservation a réussi, false sinon).
+     * @throws RemoteException
+     */
+    String creerReservation(String idTab, String idCli, int nbPers, double duree) throws RemoteException;
+
+    String getReservations(String nom, String prenom, String numTel) throws  RemoteException;
+
+    String getTables() throws RemoteException;
+}
